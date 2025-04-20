0dd860053933d1b2dac11be010d03164ed80ded1
Babszz <naija4nath@gmail.com>
2025-04-20 22:15:09 +0100

    sequelize stock trades api done

--- sequelize-stock-trades-api/routes/trades.js
+++ sequelize-stock-trades-api/routes/trades.js
@@ -1,5 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const {
+  createTrade,
+  getAllTrades,
+  getTradeById,
+  methodNotAllowed,
+} = require("../controllers/trades");
+
+router.post("/", createTrade);
+router.get("/", getAllTrades);
+router.get("/:id", getTradeById);
+router.delete("/:id", methodNotAllowed);
+router.put("/:id", methodNotAllowed);
+router.patch("/:id", methodNotAllowed);
 
 module.exports = router;
